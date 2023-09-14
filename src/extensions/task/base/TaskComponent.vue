a85991bc87ca85007d990a075b89e5516a8c8924
Kai Neubauer <moin@kaineubauer.de>
2023-09-14 16:21:35 +0200

    Add evaluation on next() call

--- src/extensions/task/base/TaskComponent.vue
+++ src/extensions/task/base/TaskComponent.vue
@@ -1,25 +1,26 @@
 <template>
   <node-view-wrapper
-    :class="[
+      :class="[
       isEditableReactive
         ? 'border border-slate-200 shadow-lg rounded-md mb-2'
         : '',
     ]"
-    class="flex w-full"
+      class="flex w-full"
   >
     <div :class="[isEditableReactive ? 'px-4 pb-2' : '']" class="flex flex-col w-full ">
       <div
-        v-if="isEditableReactive"
-        class="flex flex-row py-3 border-b border-gray-200"
-        contenteditable="false"
+          v-if="isEditableReactive"
+          class="flex flex-row py-3 border-b border-gray-200"
+          contenteditable="false"
       >
         <div class="flex flex-row grow items-center">
           <!-- eslint-disable vue/no-v-html -->
           <div data-drag-handle contenteditable="false" draggable="true" style="cursor: grab">
-            <span class="pl-1 pr-2 ml-1" v-html="calculateHexIcon(node.attrs.id)" />
+            <span class="pl-1 pr-2 ml-1" v-html="calculateHexIcon(node.attrs.id)"/>
           </div>
           <div class="flex w-full">
-            <span class="text-base font-semibold leading-6 text-gray-900">{{ $t("editor.task.type-" + node.attrs.type)
+            <span class="text-base font-semibold leading-6 text-gray-900">{{
+                $t("editor.task.type-" + node.attrs.type)
               }}</span>
           </div>
           <div class="px-1 cursor-pointer fill-gray-500 hover:fill-gray-600">
@@ -30,29 +31,30 @@
 
       <!-- Instruction -->
       <div :class="[isEditableReactive ? 'py-2': '']" class="flex flex-row w-full [&_p]:my-0">
-        <node-view-content class="w-full" />
+        <node-view-content class="w-full"/>
       </div>
 
       <!-- Task -->
       <component
-        :is="node.attrs.type"
-        :id="node.attrs.id"
-        :editor="editor"
-        :options="optionsWithDefaults"
-        :content="node.attrs.content"
-        :evaluation="node.attrs.evaluation"
-        :state="state"
-        @update="update"
-      />
-
-      <div v-if="!isEditableReactive && !!state && !['solution'].includes(state.state)" class="my-1 w-full">
-        <SubmitButton
+          :is="node.attrs.type"
           :id="node.attrs.id"
-          :type="node.attrs.type"
+          :editor="editor"
           :options="optionsWithDefaults"
+          :content="node.attrs.content"
           :evaluation="node.attrs.evaluation"
           :state="state"
-          :editor="editor"
+          @update="update"
+      />
+
+      <div v-if="!isEditableReactive && !!state && !['solution'].includes(state.state)"
+           class="my-1 w-full">
+        <SubmitButton
+            :id="node.attrs.id"
+            :type="node.attrs.type"
+            :options="optionsWithDefaults"
+            :evaluation="node.attrs.evaluation"
+            :state="state"
+            :editor="editor"
         >
         </SubmitButton>
       </div>
@@ -61,9 +63,9 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, inject, nextTick, onMounted, provide, ref, watch } from "vue";
-import { Editor, NodeViewContent, NodeViewWrapper } from "@tiptap/vue-3";
-import { calculateHexIcon } from "@/helpers/util";
+import {computed, defineComponent, inject, nextTick, onMounted, provide, ref, watch} from "vue";
+import {Editor, NodeViewContent, NodeViewWrapper} from "@tiptap/vue-3";
+import {calculateHexIcon} from "@/helpers/util";
 import IconClose from "@/helpers/icons/IconClose.vue";
 import SubmitButton from "@/extensions/task/helpers/SubmitButton.vue";
 
@@ -75,15 +77,15 @@ import FindHotspots from "@/extensions/task/find-hotspots/TaskComponent.vue";
 import Mapping from "@/extensions/task/mapping/TaskComponent.vue";
 import ShortText from "@/extensions/task/short-text/TaskComponent.vue";
 
-import type { Ref } from "vue";
-import type { EventOption } from "@/extensions/feedback/types";
-import type { NodeViewProps } from "@tiptap/core";
-import type { PropType } from "vue";
-import type { TaskContent, TaskEvaluation, TaskOptions, TaskState } from "@/extensions/task/types";
-import type { InjectedEventBus } from "@/helpers/useEventBus";
-import type { InjectedTaskStates } from "@/helpers/useTasks";
-import type { InjectedDefaults } from "@/helpers/useDefaults";
-import { evaluate } from "@/extensions/task/evaluate";
+import type {Ref} from "vue";
+import type {EventOption} from "@/extensions/feedback/types";
+import type {NodeViewProps} from "@tiptap/core";
+import type {PropType} from "vue";
+import type {TaskContent, TaskEvaluation, TaskOptions, TaskState} from "@/extensions/task/types";
+import type {InjectedEventBus} from "@/helpers/useEventBus";
+import type {InjectedTaskStates} from "@/helpers/useTasks";
+import type {InjectedDefaults} from "@/helpers/useDefaults";
+import {evaluate} from "@/extensions/task/evaluate";
 
 export type InjectedSubmit = {
   submit: (state: TaskState) => void;
@@ -143,11 +145,11 @@ export default defineComponent({
     }
   },
 
-  setup: function(props) {
-    const { taskOptions } = inject("defaults") as InjectedDefaults;
-    const { eventBus } = inject("eventBus") as InjectedEventBus;
-    const { taskStates, renderedTasks, addTaskState, updateTaskState, removeTaskState } = inject(
-      "tasks"
+  setup: function (props) {
+    const {taskOptions} = inject("defaults") as InjectedDefaults;
+    const {eventBus} = inject("eventBus") as InjectedEventBus;
+    const {taskStates, renderedTasks, addTaskState, updateTaskState, removeTaskState} = inject(
+        "tasks"
     ) as InjectedTaskStates;
 
     const state: Ref<TaskState | undefined> = computed(() => {
@@ -177,7 +179,7 @@ export default defineComponent({
       // eslint-disable-next-line vue/no-mutating-props
       props.editor.storage.feedbacks.eventOptions = [
         ...props.editor.storage.feedbacks.eventOptions.filter(
-          (e: EventOption) => e.parent !== props.node.attrs.id
+            (e: EventOption) => e.parent !== props.node.attrs.id
         ),
         ...(!!newValues.events && Array.isArray(newValues.events) ? newValues.events : [])
       ];
@@ -245,17 +247,17 @@ export default defineComponent({
             let disabledTimer = 0;
             if (["init", "incorrect"].includes(state.value.state)) {
               disabledTimer = ((state.value.disabledTimer * optionsWithDefaults.value.disabledCheckTimer) - 1) /
-                optionsWithDefaults.value.disabledCheckTimer;
+                  optionsWithDefaults.value.disabledCheckTimer;
             } else {
               disabledTimer = ((state.value.disabledTimer * optionsWithDefaults.value.disabledNextTimer) - 1) /
-                optionsWithDefaults.value.disabledNextTimer;
+                  optionsWithDefaults.value.disabledNextTimer;
             }
-            updateTaskState(state.value, { disabledTimer: disabledTimer });
+            updateTaskState(state.value, {disabledTimer: disabledTimer});
           }
           disabledTimerLocked.value = false;
         }, 1000);
       }
-    }, { immediate: true, deep: true });
+    }, {immediate: true, deep: true});
 
     watch(optionsWithDefaults, () => {
       calculateTimerCount();
@@ -273,20 +275,20 @@ export default defineComponent({
     const submit = async (state: TaskState) => {
 
       // Evaluate answer
-      const { response, facts } = await evaluate(
-        props.node.attrs.type,
-        props.node.attrs.evaluation,
-        state
+      const {response, facts} = await evaluate(
+          props.node.attrs.type,
+          props.node.attrs.evaluation,
+          state
       );
 
       // Set the next state depending on the result and the configuration of the task
       let newState;
       if (response) {
-        newState = { ...state, state: "correct" };
+        newState = {...state, state: "correct"};
       } else {
         if (
-          optionsWithDefaults.value.hasMaxAttempts &&
-          state.attempt >= optionsWithDefaults.value.maxAttempts
+            optionsWithDefaults.value.hasMaxAttempts &&
+            state.attempt >= optionsWithDefaults.value.maxAttempts
         ) {
           newState = {
             ...state,
@@ -332,13 +334,17 @@ export default defineComponent({
 
     const next = () => {
 
+      if (!!state.value?.state && ['init', 'incorrect'].includes(state.value.state)) {
+        submit(state.value)
+      }
+
       // Emit event
       eventBus.emit("interaction", {
         type: "next-clicked",
         parent: null,
         facts: {},
         data: {},
-        label: { message: "global.event.type-next-clicked" }
+        label: {message: "global.event.type-next-clicked"}
       });
     };
 
@@ -350,11 +356,17 @@ export default defineComponent({
         parent: null,
         facts: {},
         data: {},
-        label: { message: "global.event.type-feedback-clicked" }
+        label: {message: "global.event.type-feedback-clicked"}
       });
     };
 
-    provide("submit", { submit, next, feedback, submittedTaskStates, disabledTimer: state.value?.disabledTimer });
+    provide("submit", {
+      submit,
+      next,
+      feedback,
+      submittedTaskStates,
+      disabledTimer: state.value?.disabledTimer
+    });
 
     const isEditableReactive: Boolean = inject("isEditableReactive", props.editor.isEditable);
 
