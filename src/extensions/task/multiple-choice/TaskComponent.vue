12a4ba6a8677d9810e6cc094f3c12aaf27f01830
Kai Neubauer <moin@kaineubauer.de>
2023-10-24 14:35:04 +0200

    Fix pluralization on MC-task.

--- src/extensions/task/multiple-choice/TaskComponent.vue
+++ src/extensions/task/multiple-choice/TaskComponent.vue
@@ -4,32 +4,30 @@
     <template #render>
       <div v-for="index in state?.order" :key="index" class="gap-2 cursor-default">
         <div
-          class="flex flex-row pl-2"
-          :class="{
+            class="flex flex-row pl-2"
+            :class="{
             'bg-green-50': showCorrectAnswerOption(content[index]),
             'bg-red-50': showIncorrectAnswerOption(content[index]),
           }"
-          @click="toggleAnswerOptionValue(content[index])"
+            @click="toggleAnswerOptionValue(content[index])"
         >
           <input
-            type="checkbox"
-            class="mx-2 my-2.5 enabled:cursor-pointer"
-            :checked="isOptionChecked(content[index])"
-            :disabled="['correct', 'final-incorrect', 'solution'].includes(state.state)"
+              type="checkbox"
+              class="mx-2 my-2.5 enabled:cursor-pointer"
+              :checked="isOptionChecked(content[index])"
+              :disabled="['correct', 'final-incorrect', 'solution'].includes(state.state)"
           />
           <div style="flex-grow: 1"
                :class="{'cursor-pointer':!['correct', 'final-incorrect', 'solution'].includes(state.state)}">
-            <InlineEditor :content="!!content ? content[index].content : undefined" />
+            <InlineEditor :content="!!content ? content[index].content : undefined"/>
           </div>
         </div>
       </div>
       <span v-if="options?.showPartiallyCorrectCount && ['incorrect'].includes(state.state)"
             class="pl-3 text-sm text-slate-400">
         {{
-          $t("editor.task.multiple-choice.label-partially-correct-count", {
-            n: numberCorrectOptions,
-            partiallyCorrect: numberCorrectlySelectedOptionsFromPreviousSubmit,
-            partiallyIncorrect: numberIncorrectlySelectedOptionsFromPreviousSubmit
+          $t("editor.task.multiple-choice.label-partially-correct-count", numberCorrectlySelectedOptionsFromPreviousSubmit, {
+            n: numberCorrectlySelectedOptionsFromPreviousSubmit,
           })
         }}
       </span>
@@ -43,44 +41,44 @@
         </div>
 
         <input
-          :checked="evaluation.solution.find((s) => s.id === option.id)?.value || false"
-          class="mx-1 my-2"
-          type="checkbox"
-          @input="toggleEvaluationOptionValue(option)"
+            :checked="evaluation.solution.find((s) => s.id === option.id)?.value || false"
+            class="mx-1 my-2"
+            type="checkbox"
+            @input="toggleEvaluationOptionValue(option)"
         />
 
         <div class="grow [&_p]:my-1">
           <InlineEditor
-            is-editor
-            :content="option.content"
-            @update:content="updateAnswerOptionContent(option, content, $event, update)"
+              is-editor
+              :content="option.content"
+              @update:content="updateAnswerOptionContent(option, content, $event, update)"
           />
         </div>
 
         <div class="min-w-fit">
           <EditorMenuButton
-            :disabled="index === 0"
-            tabindex="-1"
-            @click="moveUpOption(index, option, content, update)"
+              :disabled="index === 0"
+              tabindex="-1"
+              @click="moveUpOption(index, option, content, update)"
           >
-            <IconArrowUp />
+            <IconArrowUp/>
           </EditorMenuButton>
           <EditorMenuButton
-            :disabled="!content || index === content.length - 1"
-            tabindex="-1"
-            @click="moveDownOption(index, option, content, update)"
+              :disabled="!content || index === content.length - 1"
+              tabindex="-1"
+              @click="moveDownOption(index, option, content, update)"
           >
-            <IconArrowDown />
+            <IconArrowDown/>
           </EditorMenuButton>
           <EditorMenuButton tabindex="-1" @click="addFeedbackHint(option.id)">
-            <IconFeedback />
+            <IconFeedback/>
           </EditorMenuButton>
           <EditorMenuButton
-            :disabled="!content || content.length <= 1"
-            tabindex="-1"
-            @click="removeOption(index, content, update)"
+              :disabled="!content || content.length <= 1"
+              tabindex="-1"
+              @click="removeOption(index, content, update)"
           >
-            <IconTrash />
+            <IconTrash/>
           </EditorMenuButton>
         </div>
       </div>
@@ -90,14 +88,14 @@
         <div style="display: flex; flex-direction: row; justify-content: end">
           <div style="display: flex; gap: 1px">
             <EditorMenuButton
-              :active="options?.shuffle"
-              :on-active-click="() => update({ options: { ...options, shuffle: false } })"
-              :on-inactive-click="() => update({ options: { ...options, shuffle: true } })"
+                :active="options?.shuffle"
+                :on-active-click="() => update({ options: { ...options, shuffle: false } })"
+                :on-inactive-click="() => update({ options: { ...options, shuffle: true } })"
             >
-              <IconShuffle />
+              <IconShuffle/>
             </EditorMenuButton>
             <EditorMenuButton @click="addOption(content, update)">
-              <IconAdd />
+              <IconAdd/>
             </EditorMenuButton>
           </div>
         </div>
@@ -108,16 +106,16 @@
     <template #evaluation>
       <!--suppress JSUnresolvedFunction -->
       <OptionsFormEnum
-        v-if="!!evaluation"
-        name="evaluationName"
-        :value="evaluation.name"
-        :options="
+          v-if="!!evaluation"
+          name="evaluationName"
+          :value="evaluation.name"
+          :options="
           evaluationOptions.map((o) => {
             return { value: o.name, label: o.label };
           })
         "
-        :label="$t('editor.task.evaluation-label-type')"
-        @update:value="updateEvaluationName"
+          :label="$t('editor.task.evaluation-label-type')"
+          @update:value="updateEvaluationName"
       />
     </template>
 
@@ -125,25 +123,25 @@
     <template #options>
       <div v-if="options" class="mt-1 flex flex-col gap-1.5">
         <OptionsFormBoolean
-          v-if="options?.showPartiallyCorrectCount !== undefined"
-          :label="$t('editor.task.multiple-choice.config-label-show-partially-correct-count')"
-          :value="options.showPartiallyCorrectCount"
-          name="showPartiallyCorrectCount"
-          @update:value="update({ options: { ...options, showPartiallyCorrectCount: $event }})"
+            v-if="options?.showPartiallyCorrectCount !== undefined"
+            :label="$t('editor.task.multiple-choice.config-label-show-partially-correct-count')"
+            :value="options.showPartiallyCorrectCount"
+            name="showPartiallyCorrectCount"
+            @update:value="update({ options: { ...options, showPartiallyCorrectCount: $event }})"
         ></OptionsFormBoolean>
         <OptionsDefaults
-          :options="options"
-          allow-empty-answer-submission
-          has-max-attempts
-          has-disabled-check-timer
-          has-disabled-next-timer
-          has-submit-button
-          has-feedback-button
-          has-next-button
-          has-correct-state
-          has-incorrect-state
-          has-final-incorrect-state
-          @update:options="update({ options: $event })"
+            :options="options"
+            allow-empty-answer-submission
+            has-max-attempts
+            has-disabled-check-timer
+            has-disabled-next-timer
+            has-submit-button
+            has-feedback-button
+            has-next-button
+            has-correct-state
+            has-incorrect-state
+            has-final-incorrect-state
+            @update:options="update({ options: $event })"
         />
       </div>
     </template>
@@ -151,7 +149,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, inject, nextTick } from "vue";
+import {computed, defineComponent, inject, nextTick} from "vue";
 
 import EditorMenuButton from "@/helpers/EditorMenuButton.vue";
 import IconAdd from "@/helpers/icons/IconAdd.vue";
@@ -166,21 +164,21 @@ import OptionsDefaults from "@/extensions/task/helpers/OptionsDefaults.vue";
 import OptionsFormBoolean from "@/extensions/task/helpers/OptionsFormBoolean.vue";
 import OptionsFormEnum from "@/extensions/task/helpers/OptionsFormEnum.vue";
 
-import { calculateHexIcon } from "@/helpers/util";
-import { formatOptions } from "@/extensions/task/multiple-choice/format/options";
-import { formatContent } from "@/extensions/task/multiple-choice/format/content";
-import { formatState } from "@/extensions/task/multiple-choice/format/state";
-import { formatEvents } from "@/extensions/task/multiple-choice/format/events";
+import {calculateHexIcon} from "@/helpers/util";
+import {formatOptions} from "@/extensions/task/multiple-choice/format/options";
+import {formatContent} from "@/extensions/task/multiple-choice/format/content";
+import {formatState} from "@/extensions/task/multiple-choice/format/state";
+import {formatEvents} from "@/extensions/task/multiple-choice/format/events";
 import {
   formatEvaluation,
   evaluationOptions
 } from "@/extensions/task/multiple-choice/format/evaluation";
-import { isEqual } from "lodash-es";
-import { useListOptions } from "@/extensions/task/helpers/listOptions";
-import { useTask } from "@/extensions/task/helpers";
-import { v4 as uuid } from "uuid";
+import {isEqual} from "lodash-es";
+import {useListOptions} from "@/extensions/task/helpers/listOptions";
+import {useTask} from "@/extensions/task/helpers";
+import {v4 as uuid} from "uuid";
 
-import type { PropType } from "vue";
+import type {PropType} from "vue";
 import type {
   MCOption,
   MCEvaluation,
@@ -189,11 +187,11 @@ import type {
   MCProps,
   MCEmits
 } from "@/extensions/task/multiple-choice/types";
-import type { Editor } from "@tiptap/vue-3";
-import type { EventTrigger, Feedback } from "@/extensions/feedback/types";
-import type { InjectedEventBus } from "@/helpers/useEventBus";
-import type { TaskState } from "@/extensions/task/types";
-import type { InjectedSubmit } from "@/extensions/task/base/TaskComponent.vue";
+import type {Editor} from "@tiptap/vue-3";
+import type {EventTrigger, Feedback} from "@/extensions/feedback/types";
+import type {InjectedEventBus} from "@/helpers/useEventBus";
+import type {TaskState} from "@/extensions/task/types";
+import type {InjectedSubmit} from "@/extensions/task/base/TaskComponent.vue";
 
 export default defineComponent({
   name: "TaskMultipleChoice",
@@ -250,37 +248,37 @@ export default defineComponent({
 
   emits: ["update", "submit"],
 
-  setup(props, { emit }) {
-    const { eventBus } = inject("eventBus") as InjectedEventBus;
-    const { submittedTaskStates } = inject("submit") as InjectedSubmit;
+  setup(props, {emit}) {
+    const {eventBus} = inject("eventBus") as InjectedEventBus;
+    const {submittedTaskStates} = inject("submit") as InjectedSubmit;
 
-    const { update } = useTask<MCProps, MCEmits, MCOptions, MCOption[], MCEvaluation, MCState>(
-      props,
-      emit,
-      [formatOptions, formatContent, formatEvaluation, formatState, formatEvents]
+    const {update} = useTask<MCProps, MCEmits, MCOptions, MCOption[], MCEvaluation, MCState>(
+        props,
+        emit,
+        [formatOptions, formatContent, formatEvaluation, formatState, formatEvents]
     );
 
-    const { addOption, removeOption, moveUpOption, moveDownOption, updateAnswerOptionContent } =
-      useListOptions<MCOption>();
+    const {addOption, removeOption, moveUpOption, moveDownOption, updateAnswerOptionContent} =
+        useListOptions<MCOption>();
 
     const toggleEvaluationOptionValue = (option: MCOption) => {
       if (!!props.evaluation && Array.isArray(props.evaluation.solution)) {
         const newSolution = props.evaluation.solution.map((s) => {
-          return s.id === option.id ? { ...s, value: !s.value } : s;
+          return s.id === option.id ? {...s, value: !s.value} : s;
         });
-        update({ evaluation: { ...props.evaluation, solution: newSolution } });
+        update({evaluation: {...props.evaluation, solution: newSolution}});
       }
     };
 
     const toggleAnswerOptionValue = async (option: MCOption) => {
       if (
-        Array.isArray(props.content) &&
-        !!props.state &&
-        !["correct", "final-incorrect"].includes(props.state.state)
+          Array.isArray(props.content) &&
+          !!props.state &&
+          !["correct", "final-incorrect"].includes(props.state.state)
       ) {
         const oldAnswer = props.state?.answer;
         const newAnswer = oldAnswer.map((s) => {
-          return s.id === option.id ? { ...s, value: !s.value } : s;
+          return s.id === option.id ? {...s, value: !s.value} : s;
         });
 
         if (!isEqual(oldAnswer, newAnswer)) {
@@ -395,7 +393,7 @@ export default defineComponent({
             evaluation: {
               name: newName,
               solution:
-                !!props.evaluation && !!props.evaluation.solution ? props.evaluation.solution : []
+                  !!props.evaluation && !!props.evaluation.solution ? props.evaluation.solution : []
             }
           });
       }
